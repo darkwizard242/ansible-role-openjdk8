b5f3c81a98950660942423fecd57161f184d50d7
Ali Muhammad <40025815+darkwizard242@users.noreply.github.com>
2020-01-26 00:35:30 -0500

    set assertion for molecule testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,23 +6,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-OPENJDK8_BINARY_PATH = '/usr/bin/java'
-OPENJDK8_PACKAGE_DEBIAN = 'openjdk-8-jdk'
-OPENJDK8_PACKAGE_EL = 'java-1.8.0-openjdk'
-
-
 def test_openjdk8_package_installed(host):
-    host.package("OPENJDK8_PACKAGE_DEBIAN").is_installed or \
-      host.package("OPENJDK8_PACKAGE_EL").is_installed
+    assert host.package("openjdk-8-jdk").is_installed or \
+      host.package("java-1.8.0-openjdk").is_installed
 
 
 def test_openjdk8_binary_exists(host):
-    host.file('OPENJDK8_BINARY_PATH').exists
-
-
-def test_openjdk8_binary_file(host):
-    host.file('OPENJDK8_BINARY_PATH').is_file
+    assert host.file('/usr/bin/java').exists
 
 
 def test_openjdk8_binary_which(host):
-    host.check_output('which java') == OPENJDK8_BINARY_PATH
+    assert host.check_output('which java') == '/usr/bin/java'
